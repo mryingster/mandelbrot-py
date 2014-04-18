761a039c1c18b46643a5e1ffe3a64a791975632b
Michael Caldwell <michael@coastalvectors.com>
2014-04-18 16:50:37 -0700

    Adding better argument interpretation. Rewording some comments. Removing uneeded functions. Cleaning up layout.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,60 +1,18 @@
 #!/usr/bin/python
 import sys, math, cairo
 
+# Exit with error message
 def die(message):
     print "ERROR:", message
     quit()
 
-if len(sys.argv) == 7:
-    xL = float(sys.argv[1])
-    yU = float(sys.argv[2])
-    xU = float(sys.argv[3])
-    yL = float(sys.argv[4])
-    #print xL, yU, xU, yL
-    if xL >= xU:
-        die("Invalid x coordinates")
-    if yL >= yU:
-        die("Invalid Y coordinates")
-    pixelWidth = int(sys.argv[5])
-    if pixelWidth <1:
-        die("Invalid image width (px)")
-    outputName=sys.argv[6]
-elif len(sys.argv) == 2:
-    #Top Left Coordinate
-    xL, yU = -2.25, 1.3 # -.29, .98 #
-    #Bottom Right Coordinate
-    xU, yL = .75, -1.3 # 0.06, .6 #
-    pixelWidth  = 512
-    outputName=sys.argv[1]
-else:
-    die ("Please provide output filename.")
-
-# Sizes - Calculate sizes
-xRange = xU - xL
-yRange = yU - yL
-pixelHeight = int(pixelWidth/xRange*yRange)
-step   = xRange / ( pixelWidth - 1.0 )
-depth  = 125
-startColor=0xff0000
-colorIncrement = 1024/depth
-
-#Setup Cairo
-surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, pixelWidth, pixelHeight)
-ctx = cairo.Context (surface)
-
+# Convert HEX value to RGB values
 def hex_to_rgb(value):
     value = "%06x"%value
     lv = len(value)
     return [int(value[i:i+lv/3], 16) for i in range(0, lv, lv/3)]
 
-def rgb_to_hex(value):
-    return value[0]*0x10000 + value[1]*0x100 + value[2]
-
-def formatColor(inputColor):
-    if inputColor == None: return "255 255 255"
-    outputColor = "%06x"%inputColor
-    return str(int(outputColor[0:2],16))+" "+str(int(outputColor[2:4],16))+" "+str(int(outputColor[4:6],16))
-
+# Generate Table of Incrementing Hues
 def genColor(startColor):
     colors=[]
     c = hex_to_rgb(startColor)
@@ -66,16 +24,79 @@ def genColor(startColor):
         elif c[0] < 255 and c[1] == 0 and c[2] == 255:                c[0]+=1  #blue to purple
         elif c[0] == 255 and c[1] == 0 and c[2] <= 255 and c[2] != 0: c[2]-=1  #purple to red
         colors.append([float(c[0]/255.0), float(c[1]/255.0), float(c[2]/255.0)])
-    return colors#float(c[0]/255.0), float(c[1]/255.0), float(c[2]/255.0)
+    return colors
 
-colorTable=genColor(startColor)
+# Setup Defualt Values
+xL, yU = -2.25, 1.3 # -.29, .98 ##Top Left Coordinate
+xU, yL = .75, -1.3 # 0.06, .6 ##Bottom Right Coordinate
+pixelWidth = 512
+startColor = 0xff0000
 
+# Parse Arguments
+skip=0
+for i in range(1, len(sys.argv)):
+    if skip > 0:
+        skip-=1
+        continue
+    if sys.argv[i] == "--coord":
+        if len(sys.argv) <= i+4:
+            die("Not enough arguments supplied.")
+        try:
+            xL = float(sys.argv[i+1])
+            yU = float(sys.argv[i+2])
+            xU = float(sys.argv[i+3])
+            yL = float(sys.argv[i+4])
+        except:
+            die("Invalid coordinates")
+        if xL >= xU:
+            die("Invalid x coordinates")
+        if yL >= yU:
+            die("Invalid Y coordinates")
+        skip=4
+    elif sys.argv[i] == "--width":
+        try:
+            pixelWidth = int(sys.argv[i+1])
+        except:
+            die("Invalid width value.")
+        if pixelWidth <1:
+            die("Invalid image width (px)")
+        skip=1
+    elif sys.argv[i] == "--color":
+        try:
+            startColor=int(sys.argv[i+1],16)
+        except:
+            die("Invalid color.")
+        skip=1
+    elif sys.argv[i] == "-o":
+        outputName=sys.argv[i+1]
+        skip=1
+    elif sys.argv[i] == "-h":
+        print "SHOW HELP"
+    else:
+        die ("Unknown option, \"%s\". Please try -h for help." % sys.argv[i])
+
+# Setup Sizes
+xRange = xU - xL
+yRange = yU - yL
+pixelHeight = int(pixelWidth/xRange*yRange)
+step = xRange / ( pixelWidth - 1.0 )
+depth = 125
+
+# Setup Color
+colorIncrement = 1024/depth
+colorTable = genColor(startColor)
+
+# Setup Cairo
+surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, pixelWidth, pixelHeight)
+ctx = cairo.Context (surface)
+
+# Mandelbrot Calculation
 def mandel(xP, yP, x, y):
     xOut = (xP ** 2) + x - (yP ** 2)
     yOut = 2 * xP * yP + y
     return xOut, yOut
 
-#Process Array
+# Process Mandelbrot Image
 for y in range(0, pixelHeight):
     for x in range(0, pixelWidth):
         xValue, yValue = (xL+(x*step)), (yU-(y*step))
@@ -95,5 +116,5 @@ for y in range(0, pixelHeight):
     print "\r%i%% Complete" % int((y*100)/pixelHeight),
     sys.stdout.flush()
 
-#Output File
+# Write Output File
 surface.write_to_png(outputName)
